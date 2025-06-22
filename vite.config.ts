5f93282f767b86d414c00b13d8bcf1b448ee564b
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2025-06-22 21:50:53 +0100

    feat: added react compiler for auto component and function memoization.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
+const ReactCompilerConfig = {
+  target: "19",
+};
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     // Makes sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
     tanstackRouter({
-      target: 'react',
+      target: "react",
       autoCodeSplitting: true,
     }),
-    react(),
-    tailwindcss()
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+    tailwindcss(),
   ],
-})
+});
