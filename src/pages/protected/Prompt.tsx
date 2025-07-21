508cb9adb7990d3c8432546bb7f4a6884a8bd9c9
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2025-07-21 03:12:29 +0100

    refactor: improve layout and styling of the Prompt component

--- src/pages/protected/Prompt.tsx
+++ src/pages/protected/Prompt.tsx
@@ -4,15 +4,15 @@ import { RootLayout } from "../../components/Layouts";
 const Prompt = () => {
   return (
     <RootLayout className="justify-center items-center flex bg-mainBg">
-      <div className="relative z-10">
-        <h2 className="font-fredoka font-semibold text-4xl sm:text-5xl text-center text-primaryDark mb-1">TrendBits</h2>
-        <h1 className="text-center mb-3 text-xl font-medium text-gray-700">What is trending on your mind?</h1>
+      <div className="relative z-10 px-5">
+        <h2 className="font-fredoka font-medium text-4xl sm:text-5xl text-center text-primaryDark mb-1">TrendBits</h2>
+        <h1 className="text-center mb-3 text-sm sm:text-xl font-medium text-gray-700">What is trending on your mind?</h1>
 
-        <div className="w-auto md:w-[560px] bg-mainBg shadow-md rounded-xl border border-[#f5f5f5]">
-          <div className="bg-transparent flex flex-col p-4 rounded-lg mb-3 gap-2">
+        <div className="w-auto md:w-[560px] bg-mainBg shadow-md rounded-xl border border-primary">
+          <div className="bg-transparent flex flex-col p-4 rounded-lg gap-2">
             <textarea
               id="prompt"
-              className="outline-none border-none w-full bg-transparent text-base text-gray-800 py-1 font-poppins resize-none h-16"
+              className="outline-none border-none w-full bg-transparent sm:text-base text-sm text-gray-800 py-1 font-poppins resize-none h-16"
               placeholder="Enter a trend or keyword (e.g., AI layoffs in 2025)"
             />
 
@@ -24,7 +24,7 @@ const Prompt = () => {
           </div>
         </div>
 
-        <p className="text-xs text-gray-500 mt-4 text-center font-poppins">
+        <p className="text-xs text-gray-500 mt-2 text-center font-poppins">
           🪄 <strong>Pro Tip:</strong> Try specific or buzzworthy trends (e.g., “AI layoffs in 2025”) to get sharper summaries.
         </p>
       </div>
